8a52bbcae008ee90842aa9b1358d58d50cb61602
hylbyj <hyldestiny@gmail.com>
2014-08-04 18:15:17 -0400

    Deadline is the first motivation

---
+++ video.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	$(".various").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+});
+
+$(document).ready(function() {
+	$('.fancybox-media').fancybox({
+		openEffect  : 'none',
+		closeEffect : 'none',
+		helpers : {
+			media : {}
+		}
+	});
+});
